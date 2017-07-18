bddabf726bdd2575bc0109db8a9ccfecf866b726
mikkie <lmj3648xm@hotmail.com>
2017-07-18 10:40:05 +0800

    disconnect

--- routes/socketHandler.js
+++ routes/socketHandler.js
@@ -4,9 +4,12 @@
 
 var handle = function (io) {
     io.on('connection', function (socket) {
-        socket.emit('pull',{"name" : "connected", "id" : socket.id});
+        socket.emit('pull',{"name" : "connection", "id" : socket.id, "type" : "connected"});
         socket.on('push', function (data) {
             socket.broadcast.emit('pull',data);
+        });
+        socket.on('disconnect', function () {
+            io.emit('pull',{"name" : "connection", "id" : socket.id, "type" : "disconnected"});
         });
     });
 };
